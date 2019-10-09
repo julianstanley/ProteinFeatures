096c55b7f2953691a9d6c8d254ed5040569381ca
julianstanley <stanley.ju@husky.neu.edu>
2019-10-09 11:17:29 -0400

    Updated comparison script to make a logfile of un-matched columns

--- resource_scripts/compare_old_new.py
+++ resource_scripts/compare_old_new.py
@@ -5,8 +5,12 @@ import pandas as pd
 old = pd.read_csv("../data/old_ref.csv")
 new = pd.read_csv("../new_compare.csv")
 
+# logging
+log = open("logfile.txt", "w")
+
 # Add a Site by Structure column to the new dataframe
-new["Site by Structure"] = new.apply(lambda x: "{protein}_{res}{loc}".format(protein = x["Protein"], res = x["AA"], loc = x["Position"].split(".")[0]), axis = 1)
+new["Site by Structure"] = new.apply(lambda x: "{protein}_{res}{loc}".format(
+    protein=x["Protein"], res=x["AA"], loc=x["Position"].split(".")[0]), axis=1)
 
 outfile = open("old_new_compare.csv", 'w')
 outfile.write("Feature,Old,New,Match\n")
@@ -15,14 +19,18 @@ for index, row in old.iterrows():
     try:
         new_match = new[new["Site by Structure"] == row["Site by Structure"]]
         if not new_match.empty:
-            outfile.write("New Structure,{},{},TRUE\n".format(row["Site by Structure"], row["Site by Structure"]))
-            for new_name in list(new_match):
-                if new_name in list(old):
-                    feature = new_name
-                    old_feature = row[new_name]
-                    new_feature = new_match[new_name].values[0]
+            outfile.write("New Structure,{},{},TRUE\n".format(
+                row["Site by Structure"], row["Site by Structure"]))
+            for old_name in list(row.keys()):
+                if old_name in list(new_match):
+                    feature = old_name
+                    old_feature = row[old_name]
+                    new_feature = new_match[old_name].values[0]
                     match = old_feature == new_feature
                     outfile.write(f'{feature},{old_feature},{new_feature},{match}\n')
+
+                else:
+                    log.write("Not matched: {}\n".format(old_name))
     except Exception as e:
         print(e)
 outfile.close()
